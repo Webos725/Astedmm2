87238f28e09fbdab63a545a14ed7556db93d1e2d
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-08-12 18:43:53 +0900

    Update font_download.py

--- script/font_download.py
+++ script/font_download.py
@@ -31,7 +31,7 @@ service = Service(executable_path="/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 try:
-    driver.get("https://www.pentacom.jp/pentacom/bitfontmaker2/gallery/?id=18783")
+    driver.get("https://www.pentacom.jp/pentacom/bitfontmaker2/gallery/?id=18810")
     time.sleep(4)
 
     # DOWNLOADボタン押下
